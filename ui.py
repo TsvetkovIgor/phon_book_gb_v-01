e9173f1ddec1702b8e3453b13c3709eecf736bb8
TsvetkovIgor <garrihome@yandex.ru>
2024-07-04 18:24:15 +0300

    added funtion inteface()

---
+++ ui.py
@@ -0,0 +1,13 @@
+def interface():
+    print("Добрый день! Вы попали на специальный бот-справочник от GeekBrains! \n 1 - записать данные. \n 2 - вывод данных")
+    command = int(input('Введите число '))
+    
+    # обработчик ошибок
+    while command != 1 and command != 2:
+        print("Неправильный ввод ")
+        command = int(input('Введите число '))
+
+    if command == 1:
+        input_data()
+    elif command == 2:
+        print_data()
